adb9822a1b2634c92bf4319e380530c5a2c13732
Khandamir <dameer.db@gmail.com>
2025-12-15 18:16:27 +0500

    FEAT 17

---
+++ src/main/java/com/example/demo/ApiController.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class ApiController {
+
+    @GetMapping("/api/time")
+    public Map<String, String> getTime() {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Hello from backend");
+        response.put(
+                "time",
+                LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+        );
+        return response;
+    }
+}
